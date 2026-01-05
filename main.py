73ef8a9fe23df810e79df44ac84af82ecda01336
lichen-brain <LiChen.brain@gmail.com>
2026-01-05 22:04:04 +0800

    feat: add horizontal_projection and vertical_projection, delete fnc(projection_character). but, the function has not yet been verified for correctness.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import pytesseract
 
 init()  # 初始化 colorama  
 image_directory = 'image'  # 图像读取目录
+ocr_config = '--psm 6'  # OCR配置参数
 
 
 # 预处理图像
@@ -42,7 +43,7 @@ def preprocess_image(image_path):
     # ret_otsu, image_otsu = cv2.threshold(image_blur, 0, 255,
     #     cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     image_adapt = cv2.adaptiveThreshold(image_gray, 255, 
-        cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 2)
+        cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 5)
     # cv2.imshow("Blurring + Otsu's Threshold", image_otsu)
     image_preprocessed = image_adapt
 
@@ -56,23 +57,75 @@ def preprocess_image(image_path):
 
     return image_preprocessed, image
 
-# 水平和垂直投影特征提取
-def projection_character(binary_image):
-    # 计算水平投影（每一行的黑白像素点的数量）
-    h_projection = np.sum(binary_image, axis=1)
-    # 计算垂直投影（每一列的黑白像素点的数量）
-    v_projection = np.sum(binary_image, axis=0)
 
-    plt.figure(figsize=(10, 5))
-    plt.subplot(1, 2, 1)
-    plt.plot(h_projection)
-    plt.title("Horizontal Projection")
 
-    plt.subplot(1, 2, 2)
-    plt.plot(v_projection)
-    plt.title("Vertical Projection")
-    plt.suptitle("projection_character")
-    plt.show()
+# 设置阈值，根据水平投影裁剪文本行区域(水平投影) 
+# return -> [[],[]...] 每个文本行的范围行的列表(二维)  else return None
+# 大列表里装的是从第几行到第几行的二维列表
+def horizontal_projection(binary_image, h_min_ratio = 0.3):# 最小比例阈值
+    h_projection = np.sum(binary_image == 0, axis=1)   # 计算水平投影（每一行的黑像素点的数量）
+    h_max_val = np.max(h_projection)   # 水平投影最大值
+    h_threshold = h_max_val * h_min_ratio  # 水平投影阈值
+    rows = np.where(h_projection >= h_threshold)[0]   # 满足阈值的行索引
+    # 没有显著的水平投影
+    if(len(rows) == 0):
+        print(Fore.RED + "No significant horizontal projection found.")
+        return None
+    h_regions = binary_image[rows[0]:rows[-1], :]   # 裁剪出的文本行区域, 列不变
+    # print(h_regions)
+    # print(h_max_val)
+    # for i in rows:
+    #     print(Fore.BLUE + f"Row {i}: Horizontal projection value = {h_projection[i]}")
+    return h_regions
+
+
+
+# 设置阈值，使用垂直投影分割字符(垂直投影) 
+# return -> [[[]],[[]]...] 每个字符的范围列的列表(三维)  else return []
+# 大列表里装的是每个字符区域的二维列表[起始列，结束列]
+def vertical_projection(binary_image, v_min_width = 5, v_min_ratio = 0.3):# 最小宽度阈值, 最小比例阈值
+    v_projection = np.sum(binary_image == 0, axis=0)   # 计算垂直投影（每一列的白像素点的数量）
+    v_max_val = np.max(v_projection)   # 垂直投影最大值
+    v_threshold = v_max_val * v_min_ratio  # 垂直投影阈值
+    cols = np.where(v_projection >= v_threshold)[0]   # 满足阈值的列索引
+    # 没有显著的垂直投影
+    if(len(cols) == 0):
+        print(Fore.RED + "No significant vertical projection found.")
+        return []
+    char_regions = []   # 存储字符区域的列表
+    v_start = cols[0]   # 字符区域起始列索引
+
+    for i in range(1, len(cols)):
+        # print(Fore.CYAN + f"Column {cols[i]}: Vertical projection value = {v_projection[cols[i]]}")
+        if cols[i] != cols[i - 1] + 1:
+            v_end = cols[i - 1]
+            if v_end - v_start > v_min_width:
+                char_regions.append([v_start, v_end])
+            v_start = cols[i]
+    # 添加最后一个字符区域
+    if cols[-1] - v_start > v_min_width:
+        char_regions.append([v_start, cols[-1]])
+    v_regions = [binary_image[:, x1:x2] for x1, x2 in char_regions]  # 裁剪出的字符区域, 每个小区域行不变
+    return v_regions
+
+
+
+# -> 已废弃, 使用horizontal_projection和vertical_projection函数
+# 水平和垂直投影特征提取     
+# def projection_character(binary_image):
+#     # 绘制水平和垂直投影图
+#     plt.figure(figsize=(10, 5))
+#     plt.subplot(1, 2, 1)
+#     plt.plot(h_projection)
+#     plt.title("Horizontal Projection")
+
+#     plt.subplot(1, 2, 2)
+#     plt.plot(v_projection)
+#     plt.title("Vertical Projection")
+#     plt.suptitle("projection_character")
+#     plt.show()
+
+
 
 # main
 def main():
@@ -91,20 +144,23 @@ def main():
         cv2.imshow("Preprocessed Image", image_preprocess)
         cv2.imshow("Contour Image", image_contour)
         # cv2.imshow("Original Image", image)
-        # 使用Tesseract进行OCR识别
-        text = pytesseract.image_to_string(image_preprocess, lang='chi_sim')
 
         print(Fore.YELLOW + "Recognized Text:")
         print(text) 
-
+        
+        # 按 'q' 键退出
         key = cv2.waitKey(0) & 0xFF
-        if key == ord('q'):  # 按 'q' 键退出
+        if key == ord('q'):  
             break
         # projection_character(image_preprocess)
+
         
+        # 使用Tesseract进行OCR识别
+        text = pytesseract.image_to_string(image_preprocess, lang='chi_sim')
    
    
     cv2.destroyAllWindows()
+
 
 
 
